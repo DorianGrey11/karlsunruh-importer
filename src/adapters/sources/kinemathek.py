25007d41aa5e0a138cd29caef22388024ef92a03
DorianGrey11 <doriangrey11@posteo.eu>
2026-02-07 22:59:04 +0100

    feat: import koop-vortrag events from kinemathek

--- src/adapters/sources/kinemathek.py
+++ src/adapters/sources/kinemathek.py
@@ -9,7 +9,7 @@ from src.adapters.sources.base import EventSource, DAYS_AHEAD_TO_REQUEST
 
 KINEMATHEK_SPIELPLAN_API_URL = "https://kinemathek-karlsruhe.de/wp-json/wp/v2/pages/9808"
 
-TAGS_TO_IMPORT = ["klima-krisen-utopien", "gemeinwohloekonomie", "koop-kino"]
+TAGS_TO_IMPORT = ["klima-krisen-utopien", "gemeinwohloekonomie", "koop-kino", "koop-vortrag"]
 
 
 def map_event_tag_to_topic(event_tag: str) -> Topic:
